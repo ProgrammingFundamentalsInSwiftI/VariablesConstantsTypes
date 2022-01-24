51ca0d2fc29664363cc4306ab8cc3d6af241657f
John Lynch <lynchjohn98@gmail.com>
2022-01-24 15:18:36 -0600

    Lab 1 Commit

--- VariablesConstantsTypes/main.swift
+++ VariablesConstantsTypes/main.swift
@@ -1,11 +1,300 @@
-//
-//  main.swift
-//  VariablesConstantsTypes
-//
-//  Created by John William Lynch on 1/24/22.
-//
-
 import Foundation
 
-print("Hello, World!")
+// PROBLEM 1.0 : CREATING A CONSTANT
+// Declare a constant called friends to represent the number of friends you have on social media. Give it a value between 50 and 1000. Print out the value by referencing your constant.
+// Write your code between ----------------------
 
+
+
+// PROBLEM 1.1 : REMOVING FRIENDS
+// ----------------------------------------------
+// Now assume you go through and remove friends that aren't active on social media. Attempt to update your friends constant to a lower number than it currently is. Observe what happens and then move to the next step.
+// Write your code between ----------------------
+
+
+// ----------------------------------------------
+
+// PROBLEM 1.2 : CONSTANT ERRORS
+// Does the above code compile? Why not? Print your explanation to the console using the print function. Go back and comment out your line of code that updates the friends constant to a lower number so that you lab will compile properly.
+// Write your code between ----------------------
+
+
+// ----------------------------------------------
+
+
+// PROBLEM 2.0 : FITNESS APP CREATION
+// For the questions below, imagine that you are developing a fitness app.
+// Write your code between ----------------------
+
+
+
+
+// ----------------------------------------------
+
+
+
+// Your fitness tracking app needs to know goal number of steps per day. Create a constant goalSteps and set it to 10000
+// Write your code between ----------------------
+
+// ----------------------------------------------
+
+
+
+
+// PROBLEM 2.0 : FITNESS APP OUTPUT
+//Use two print functions to print two separate lines to the console. The first line should say "Your step goal for the day is:", and the second line should print the value of goalSteps by referencing your constant.
+// Write your code between ----------------------
+
+
+
+
+
+// PROBLEM 3.0 : SCHOOL YEARS
+// ----------------------------------------------
+// Declare a variable schooling and set it to the number of years of school that you have completed. Print schooling to the console.
+// Write your code between ----------------------
+
+
+
+
+
+
+// ----------------------------------------------
+// Now imagine you just completed an additional year of school, and update the schooling variable accordingly. Print schooling to the console.
+// Write your code between ----------------------
+
+
+
+
+
+
+// PROBLEM 3.1 : SCHOOL YEARS ERROR
+// ----------------------------------------------
+// Does the above code compile? Why is this different than trying to update a constant? Print your explanation to the console using the print function.
+// Write your code between ----------------------
+
+
+
+
+
+
+
+
+// ----------------------------------------------
+// PROBLEM 4.0 : STEP TRACKER
+// Create a variable called steps that will keep track of the number of steps you take throughout the day. Set its initial value to 0 to represent the step count first thing in the morning. Print steps to the console.
+// Write your code between ----------------------
+
+
+
+
+
+
+
+
+
+
+
+// ----------------------------------------------
+// PROBLEM 4.1 : STEP INCREMENT
+// ----------------------------------------------
+// Now assume the tracker has been keeping track of steps all morning, and you want to show the user the latest step count.
+// Write your code between ----------------------
+
+
+
+
+
+
+
+
+
+// ----------------------------------------------
+// PROBLEM 5.0 : PHOTO SHARING APP
+/*
+ Imagine you're creating a simple photo sharing app. You want to keep track of the following metrics for each post:
+
+ Number of likes: the number of likes that a photo has received
+ Number of comments: the number of comments other users have left on the photo
+ Year created: The year the post was created
+ Month created: The month the post was created represented by a number between 1 and 12
+ Day created: The day of the month the post was created
+
+ For each of the metrics above, declare either a constant or a variable and assign it a value corresponding to a hypothetical post. Be sure to use proper naming conventions.
+ */
+// Write your code between ----------------------
+
+
+
+
+
+
+
+
+
+
+
+// ----------------------------------------------
+// PROBLEM 6.0 : DECIMAL CREATION
+// Declare two variables, one called firstDecimal and one called secondDecimal. Both should have decimal values. Look at both of their types by holding Option and clicking on the variable name.
+// Write your code between ----------------------
+
+
+
+
+
+
+
+// ----------------------------------------------
+// PROBLEM 6.1 : BOOLEAN CREATION
+// ----------------------------------------------
+// Declare a variable called trueOrFalse and give it a boolean value. Try to assign it to firstDecimal like so: firstDecimal = trueOrFalse. Does it compile? Print a statement to the console explaining why not, and remove the line of code that will not compile.
+// Write your code between ----------------------
+
+
+
+
+
+
+
+
+
+// ----------------------------------------------
+// PROBLEM 6.2 : TYPE ERRORS
+// ----------------------------------------------
+// Declare a variable and give it a string value. Then try to assign it to firstDecimal. Does it compile? Print a statement to the console explaining why not, and remove the line of code that will not compile.
+// Write your code between ----------------------
+
+
+
+
+
+
+
+// ----------------------------------------------
+// PROBLEM 6.3 : TYPE ERRORS
+// ----------------------------------------------
+//Finally, declare a variable with a whole number value. Then try to assign it to firstDecimal. Why won't this compile even though both variables are numbers? Print a statement to the console explaining why not, and remove the line of code that will not compile.
+// Write your code between ----------------------
+
+
+
+
+
+
+
+
+
+
+
+
+// ----------------------------------------------
+// PROBLEM 7.0 : STEP CONSTANTS
+// You have declared a number of constants and variables to keep track of fitness information. Declare one more variable with a boolean value called hasMetStepGoal.
+// Write your code between ----------------------
+
+
+
+
+
+
+
+
+
+
+// ----------------------------------------------
+// PROBLEM 7.1 : STEP REDECLARE
+// When you declared a constant for goal number of steps and a variable for current step count, you likely assigned each a value in the thousands. This can be difficult to read. Redeclare this constant and variable and, when assigning each a value in the thousands, format the number so that it is more readable.
+// Write your code between ----------------------
+
+
+
+
+
+
+
+// ----------------------------------------------
+// PROBLEM 8.0 : STRING TYPES
+// ----------------------------------------------
+// Declare a variable called name of type String, but do not give it a value. Print name to the console. Does the code compile? Remove any code that will not compile.
+// Write your code between ----------------------
+
+
+
+
+
+
+
+
+
+// ----------------------------------------------
+// PROBLEM 8.1 : STRING USAGE
+// Now assign a value to name, and print it to the console.
+// Write your code between ----------------------
+
+
+
+
+
+
+
+
+
+// ----------------------------------------------
+// PROBLEM 9.0 : VARIABLE WORK
+// Declare a variable called distanceTraveled and set it to 0. Do not give it an explicit type.
+// Write your code between ----------------------
+
+
+
+
+
+
+
+
+
+// ----------------------------------------------
+// PROBLEM 9.1 : VARIABLE WORK
+// ----------------------------------------------
+// Now assign a value of 54.3 to distanceTraveled. Does the code compile? Go back and set an explicit type on distanceTraveled so the code will compile.
+// Write your code between ----------------------
+
+
+
+
+
+
+
+
+
+// ----------------------------------------------
+// PROBLEM 10.0 : FITNESS APP FINALIZATION
+// ----------------------------------------------
+// You decide that your fitness tracking app should show the user what percentage of his/her goal has been achieved so far today. Declare a variable called percentCompleted and set it to 0. Do not explicity assign it a type.
+// Write your code between ----------------------
+
+
+
+
+
+
+
+
+
+
+// ----------------------------------------------
+// PROBLEM 10.1 : FITNESS APP FINALIZATION
+// ----------------------------------------------
+// Imagine that partway through the day a user has taken 3,467 steps out of the 10,000 step goal. This means he/she is 34.67% of the way to his/her goal. Assign 34.67 to percentCompleted. Does the code compile? Go back and explicity assign a type to percentCompleted that will allow the code to compile.
+// Write your code between ----------------------
+
+
+
+
+
+
+
+
+
+
+// ----------------------------------------------
